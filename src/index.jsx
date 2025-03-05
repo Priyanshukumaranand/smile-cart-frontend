d88d030dd02f7f6ce00bae9364cb5dbeec051a83
Priyanshu Kumar Anand <118902541+Priyanshukumaranand@users.noreply.github.com>
2025-03-05 19:35:56 +0000

    Initialized i18n

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-
+import "./common/i18n";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
